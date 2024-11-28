76e6b3dc4e936527c984b905895ef26875e5e141
Kishore K <kishore.20ee@kct.ac.in>
2024-11-28 15:48:55 +0530

    sitemap added

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://xennium.org', // Replace with your actual website URL
+    generateRobotsTxt: true, // Generates a robots.txt file for SEO purposes
+  };
+  
