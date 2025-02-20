d50b69d881deeb0e8754df382bb26091908fb92a
Kishore K <kishore.20ee@kct.ac.in>
2025-02-20 13:45:49 +0530

    njs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: "/:path*",
-          destination: "https://xennium.org/:path*",
-          permanent: true,
-        },
-        {
-          source: "/www/:path*",
-          destination: "https://xennium.org/:path*",
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
